d1eb8be2225cc0a247f2812e69501c12fdaedf09
Evgen Zaharevich <metrovyy@gmail.com>
2023-01-18 19:56:34 +0200

    4ek

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/API';
 import './js/gallery';
+import './js/lightbox';
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,18 +1,96 @@
 import { fetchImages, PER_PAGE } from './API';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { lightbox } from './lightbox';
 
 const formRef = document.querySelector(`.search-form`);
+const galleryRef = document.querySelector(`.gallery`);
+const loadMoreBtnRef = document.querySelector(`.load-more`, onLoadMore);
 
 let currentPage = 1;
 
+formRef.addEventListener(`submit`, onDeleteMarkup);
 formRef.addEventListener(`submit`, onSearch);
+loadMoreBtnRef.addEventListener(`click`, onLoadMore);
 
 async function onSearch(e) {
   e.preventDefault();
 
   const { searchQuery } = e.target;
 
-  const response = await fetchImages(searchQuery.value, currentPage);
+  if (!searchQuery.value) {
+    Notify.info(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    return;
+  }
+
+  const response = await fetchImages(
+    searchQuery.value.trim().toLowerCase(),
+    currentPage
+  );
+
+  onShowNotificationForHitsValue(response);
+
+  onAddMarkupOnDOM(onCreateMarkupForCardImage(response));
+  lightbox.refresh();
+}
+
+function onCreateMarkupForCardImage(response) {
+  const { hits } = response.data;
+  return hits.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card">
+        <a class="gallery__link" href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${downloads}
+          </p>
+        </div>
+      </div>
+      `;
+    }
+  );
+}
+
+function onAddMarkupOnDOM(markup) {
+  galleryRef.insertAdjacentHTML(`afterbegin`, markup);
+}
+
+function onDeleteMarkup() {
+  galleryRef.innerHTML = ` `;
+  hiddenBtn();
+}
+
+function hiddenBtn() {
+  loadMoreBtnRef.classList.add('visually-hidden');
+  loadMoreBtnRef.setAttribute('disabled', '');
+}
+
+function showBtn() {
+  loadMoreBtnRef.removeAttribute('disabled');
+  loadMoreBtnRef.classList.remove('visually-hidden');
+}
+
+function onShowNotificationForHitsValue(response) {
   const { hits, totalHits } = response.data;
 
   if (hits.length === 0) {
@@ -21,5 +99,38 @@ async function onSearch(e) {
     );
   } else {
     Notify.success(`Hooray! We found ${totalHits} images.`);
+
+    showBtn();
   }
 }
+
+async function onLoadMore(e) {
+  currentPage += 1;
+
+  const response = await fetchImages(formRef.searchQuery.value, currentPage);
+
+  await onAddCardsForPressLoadMoreBtn(onCreateMarkupForCardImage(response));
+
+  onShowNotificationForHitsValue(response);
+  lightbox.refresh();
+
+  if (galleryRef.children.length === response.data.totalHits) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    hiddenBtn();
+  }
+}
+
+function onAddCardsForPressLoadMoreBtn(markup) {
+  return galleryRef.insertAdjacentHTML('beforeend', markup);
+}
+
+// function onScroll() {
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
+
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: 'smooth',
+//   });
+// }
